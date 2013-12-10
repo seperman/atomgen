8f6cea6ea858103b0394e0c827cc476722d523f5
Sep Dehpour <sdehpour@zehnergroup.com>
2013-12-09 23:21:06 -0800

    Updating setup

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup #, find_packages
+
+with open('README.md') as file:
+    long_description = file.read()
 
 setup(name='atomgen',
       version='0.1',
@@ -10,6 +13,7 @@ setup(name='atomgen',
       license='MIT',
       packages=['atomgen'],
       zip_safe=False,
+      long_description=long_description,
       classifiers=[
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
