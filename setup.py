e85ee90c4535c7b07aa719f0ee97731480d92d6b
Sep Dehpour <sdehpour@zehnergroup.com>
2013-12-09 18:28:44 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='atomgen',
+      version='0.1',
+      description='Creates Apple Newsstand Atom Feed',
+      url='https://github.com/erasmose/atomgen',
+      download_url='https://github.com/erasmose/atomgen/tarball/master',
+      author='Erasmose',
+      author_email='xpower3d@yahoo.com',
+      license='MIT',
+      packages=['atomgen'],
+      zip_safe=False,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+        ],
+      )
