b062a88288b2091402caac861fb2e02638e32eaa
Sep Dehpour <sdehpour@zehnergroup.com>
2013-12-09 21:26:05 -0800

    Updating the func

--- atomgen.py
+++ atomgen.py
@@ -84,7 +84,7 @@ class AtomGen(object):
         Example:
             >>> a=[{'id':'1','updated':datetime.datetime(2013, 12, 10, 1, 9, 53, 977342),'published':datetime.datetime(2013, 12, 10, 1, 10, 53, 977342),'summary':"This is the summary 1",'SOURCE':"http://ccc.com/img.png"},{'id':2,'updated':datetime.datetime(2013, 12, 9, 1, 9, 53, 977342),'published':datetime.datetime(2013, 12, 10, 1, 7, 53, 977342),'summary':"This is the summary 2",'SOURCE':"http://ccc2.com/img2.png"}]
             >>> my_atom = AtomGen()
-            >>> my_atom.run(a, update_time=datetime.datetime(2013, 12, 10, 1, 9, 53, 977342))
+            >>> print my_atom.run(a, update_time=datetime.datetime(2013, 12, 10, 1, 9, 53, 977342))
             <?xml version='1.0' encoding='UTF-8'?>
             <feed xmlns="http://www.w3.org/2005/Atom" xmlns:news="http://itunes.apple.com/2011/Newsstand"><updated>2013-12-10T01:09:53Z</updated><entry><id>1</id><updated>2013-12-10T01:09:53Z</updated><published>2013-12-10T01:10:53Z</published><summary>This is the summary 1</summary><news:cover_art_icons><news:cover_art_icon size="SOURCE" src="http://ccc.com/img.png" /></news:cover_art_icons></entry><entry><id>2</id><updated>2013-12-09T01:09:53Z</updated><published>2013-12-10T01:07:53Z</published><summary>This is the summary 2</summary><news:cover_art_icons><news:cover_art_icon size="SOURCE" src="http://ccc2.com/img2.png" /></news:cover_art_icons></entry></feed>
 
@@ -92,7 +92,7 @@ class AtomGen(object):
         Example of redefining element names:
             >>> b=[{'my_id':'1','when_updated':datetime.datetime(2013, 12, 10, 1, 9, 53, 977342),'when_published':datetime.datetime(2013, 12, 10, 1, 10, 53, 977342),'the_summary':"This is the summary 1",'icon':"http://ccc.com/img.png"},{'my_id':2,'when_updated':datetime.datetime(2013, 12, 9, 1, 9, 53, 977342),'when_published':datetime.datetime(2013, 12, 10, 1, 7, 53, 977342),'the_summary':"This is the summary 2",'icon':"http://ccc2.com/img2.png"}]
             >>> my_atom2 = AtomGen(id="my_id",published="when_published",updated="when_updated",summary="the_summary",SOURCE="icon")
-            >>> my_atom2.run(b, update_time=datetime.datetime(2013, 12, 10, 1, 9, 53, 977342))
+            >>> print my_atom2.run(b, update_time=datetime.datetime(2013, 12, 10, 1, 9, 53, 977342))
             <?xml version='1.0' encoding='UTF-8'?>
             <feed xmlns="http://www.w3.org/2005/Atom" xmlns:news="http://itunes.apple.com/2011/Newsstand"><updated>2013-12-10T01:09:53Z</updated><entry><id>1</id><updated>2013-12-10T01:09:53Z</updated><published>2013-12-10T01:10:53Z</published><summary>This is the summary 1</summary><news:cover_art_icons><news:cover_art_icon size="SOURCE" src="http://ccc.com/img.png" /></news:cover_art_icons></entry><entry><id>2</id><updated>2013-12-09T01:09:53Z</updated><published>2013-12-10T01:07:53Z</published><summary>This is the summary 2</summary><news:cover_art_icons><news:cover_art_icon size="SOURCE" src="http://ccc2.com/img2.png" /></news:cover_art_icons></entry></feed>
 
@@ -137,8 +137,8 @@ class AtomGen(object):
                 icon_source.set("src",item[self.img_source])
 
 
-        print ET.tostring(feed, encoding="UTF-8", method="xml")
-
+        result = ET.tostring(feed, encoding="UTF-8", method="xml")
+        return result
 
 
 if __name__ == "__main__":
